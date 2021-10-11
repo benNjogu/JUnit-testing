0fc5cc4bb417f6a4b1ee2af65f7d0fd77015cb4b
benNjogu <kamandemachafi.gmail.com>
2021-10-11 22:53:18 +0300

    initial commit. assert equals error

---
+++ JUnitTesting/src/main/java/com/keytech/StringHelper.java
@@ -0,0 +1,29 @@
+package com.keytech;
+
+public class StringHelper {
+
+	//AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+	public String truncateAInFirstTwoPositions(String str) {
+		if (str.length() <= 2)
+			return str.replace("A", "");
+
+		String first2Chars = str.substring(0, 2);
+		String StringMinusFirst2Chars = str.substring(2);
+
+		return first2Chars.replaceAll("A", "") + StringMinusFirst2Chars;
+
+	}
+
+	public boolean areFirstAndLastTwoCharactersTheSame(String str) {
+		if (str.length() <= 1)
+			return false;
+		if (str.length() == 2)
+			return true;
+
+		String firstTwoChars = str.substring(0, 2);
+		String lastTwoChars = str.substring(str.length() - 2);
+		return firstTwoChars.equals(lastTwoChars);
+
+	}
+
+}
---
+++ JUnitTesting/src/test/java/com/keytech/StringHelperTest.java
@@ -0,0 +1,17 @@
+package com.keytech;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class StringHelperTest {
+
+	@Test
+	public void test() {
+		assertEquals("ABC", "ABCD");
+		//expected, actual-->this are the arguments above;
+	}
+
+}
