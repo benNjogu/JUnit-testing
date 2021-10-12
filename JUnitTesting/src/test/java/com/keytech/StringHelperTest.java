4f6677c27f8a6796e2403df5b3607738bb04c8cd
benNjogu <kamandemachafi.gmail.com>
2021-10-12 15:26:35 +0300

    refactoring our unit test to best practices

--- JUnitTesting/src/test/java/com/keytech/StringHelperTest.java
+++ JUnitTesting/src/test/java/com/keytech/StringHelperTest.java
@@ -6,14 +6,19 @@ import org.junit.Test;
 
 public class StringHelperTest {
 
+	StringHelper helper = new StringHelper();
+
 	@Test
-	public void test() {
-		//AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
-		StringHelper helper = new StringHelper();
+	public void testTruncateAInFirstTwoPositions_AInFirstTwoPositions() {
+		// AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
 		assertEquals("CD", helper.truncateAInFirstTwoPositions("AACD"));
+
+		// expected, actual-->this are the arguments above;
+	}
+
+	@Test
+	public void testTruncateAInFirstTwoPositions2_AInTheFirstPositionOnly() {
 		assertEquals("CD", helper.truncateAInFirstTwoPositions("ACD"));
-		
-		//expected, actual-->this are the arguments above;
 	}
 
 }
