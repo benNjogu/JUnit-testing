6ae4c38df599c975b6f1d47b13daa067b7df1803
benNjogu <kamandemachafi.gmail.com>
2021-10-13 23:05:03 +0300

    parameterized tests

---
+++ JUnitTesting/src/test/java/com/keytech/StringHelperParameterizedTest.java
@@ -0,0 +1,57 @@
+package com.keytech;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class StringHelperParameterizedTest {
+
+	StringHelper helper = new StringHelper();
+	
+	private String input;
+	private String expectedOutput;
+
+	public StringHelperParameterizedTest(String input, String expectedOutput) {
+		super();
+		this.input = input;
+		this.expectedOutput = expectedOutput;
+	}
+
+	@Parameters
+	public static Collection<String[]> testConditions() {
+		String expectedOutPuts[][] = { { "AACD", "CD" }, { "ACD", "CD" } };
+		return Arrays.asList(expectedOutPuts);
+	}
+
+	@Test
+	public void testTruncateAInFirstTwoPositions_AInFirstTwoPositions() {
+		// AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+		assertEquals(expectedOutput, helper.truncateAInFirstTwoPositions(input));
+
+		// expected, actual-->this are the arguments above;
+	}
+
+	@Test
+	public void testTruncateAInFirstTwoPositions2_AInTheFirstPositionOnly() {
+		assertEquals(expectedOutput, helper.truncateAInFirstTwoPositions(input));
+	}
+
+	// @Test
+	// public void testTruncateAInNoPosition3_NoAinAnyPosition() {
+	// assertEquals("CDEF", helper.truncateAInFirstTwoPositions("CDEF"));
+	// }
+	//
+	// @Test
+	// public void testTruncateAInLastTwoPositions_AinLastTwoPositions() {
+	// assertEquals("CDAA", helper.truncateAInFirstTwoPositions("CDAA"));
+	// }
+
+}
