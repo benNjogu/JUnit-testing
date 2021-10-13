a323c7cfb7fcc141f8f3ff25c291a4035840dae6
benNjogu <kamandemachafi.gmail.com>
2021-10-13 20:43:24 +0300

    @beforeClass

--- JUnitTesting/src/test/java/com/keytech/QuickBeforeAfterTest.java
+++ JUnitTesting/src/test/java/com/keytech/QuickBeforeAfterTest.java
@@ -4,39 +4,34 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class QuickBeforeAfterTest {
 
+	@BeforeClass
+	public static void BeforeClass() {
+		System.out.println("before class");
+	}
+	
 	@Before
 	public void setup() {
 		System.out.println("Before test");
 	}
-	
+
 	@Test
 	public void test1() {
 		System.out.println("Test 1 executed");
 	}
-	
+
 	@Test
 	public void test2() {
 		System.out.println("Test 2 executed");
 	}
-	
+
 	@After
 	public void TearDown() {
 		System.out.println("After test");
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
