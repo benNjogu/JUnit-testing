8495ce7b60d52c2006b4fc6d13c9bb6b7edd6859
benNjogu <kamandemachafi.gmail.com>
2021-10-14 13:14:29 +0300

    My initial commit

---
+++ mockito-course/src/test/java/com/keytech/FirstMockitoTest.java
@@ -0,0 +1,16 @@
+package com.keytech;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
